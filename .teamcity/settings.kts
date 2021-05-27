11598c08c6c086407502b55b8f8fd861e82d88dc
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-05-27 17:09:36 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -72,7 +72,7 @@ object ResharperDuplicates : BuildType({
         step {
             type = "dotnet-tools-dupfinder"
             param("dotnet-tools-dupfinder.hashing.discard_local_variables_name", "true")
-            param("dotnet-tools-dupfinder.hashing.discard_types", "true")
+            param("dotnet-tools-dupfinder.hashing.discard_types", "false")
             param("jetbrains.resharper-clt.clt-path", "%teamcity.tool.jetbrains.resharper-clt.DEFAULT%")
         }
     }
