4cdf1c859774ac5bc2be9f06c99e9eeef44c1596
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-05-27 19:00:29 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -7,38 +7,31 @@ The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
 project() function with a Project instance or an init function as
 an argument.
-
 VcsRoots, BuildTypes, Templates, and subprojects can be
 registered inside the project using the vcsRoot(), buildType(),
 template(), and subProject() methods respectively.
-
 To debug settings scripts in command-line, run the
-
     mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
 command and attach your debugger to the port 8000.
-
 To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 -> Tool Windows -> Maven Projects), find the generate task node
 (Plugins -> teamcity-configs -> teamcity-configs:generate), the
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2021.1"
+version = "2020.2"
 
 project {
 
     vcsRoot(HttpsGithubComThirusabariCSharpSamplesForBeginners)
 
-    buildType(ResharperDuplicates)
-
-    params {
-        param("s2", "s")
-    }
+    buildType(B)
 }
 
-object ResharperDuplicates : BuildType({
-    name = "resharper duplicates"
+object B : BuildType({
+    name = "b"
+
+    artifactRules = "+:../temp/buildTmp/* => ./"
 
     vcs {
         root(HttpsGithubComThirusabariCSharpSamplesForBeginners)
@@ -48,8 +41,9 @@ object ResharperDuplicates : BuildType({
         step {
             name = "Resharper - Duplicates"
             type = "dotnet-tools-dupfinder"
+            param("dotnet-tools-dupfinder.discard_cost", "60")
             param("dotnet-tools-dupfinder.hashing.discard_local_variables_name", "true")
-            param("dotnet-tools-dupfinder.hashing.discard_fields_name", "true")
+            param("dotnet-tools-dupfinder.hashing.discard_fields_name", "false")
             param("dotnet-tools-dupfinder.exclude_files", """
                 **/*.Designer.cs
                 Database/**
@@ -62,18 +56,13 @@ object ResharperDuplicates : BuildType({
                 **/LpSolveApi.cs
                 **/*.generated.cs
             """.trimIndent())
-            param("dotnet-tools-dupfinder.hashing.discard_types", "true")
+            param("dotnet-tools-dupfinder.hashing.discard_types", "false")
             param("dotnet-tools-dupfinder.hashing.normalize_types", "true")
             param("jetbrains.resharper-clt.clt-path", "%teamcity.tool.jetbrains.resharper-clt.DEFAULT%")
         }
         script {
-            scriptContent = "ping 127.0.0.1 -n 60 > nul"
-        }
-        step {
-            type = "dotnet-tools-dupfinder"
-            param("dotnet-tools-dupfinder.hashing.discard_local_variables_name", "true")
-            param("dotnet-tools-dupfinder.hashing.discard_types", "true")
-            param("jetbrains.resharper-clt.clt-path", "%teamcity.tool.jetbrains.resharper-clt.DEFAULT%")
+            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            scriptContent = "ping -n 120 localhost > NUL"
         }
     }
 })
