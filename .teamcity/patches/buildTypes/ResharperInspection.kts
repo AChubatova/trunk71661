bc18185f0fe3b3cadc7aa43460eeb5b88d6e3a72
admin <admin@TeamCity>
2021-06-22 15:39:35 +0200

    TeamCity change in 'resharper duplicates' project: VCS roots of 'resharper inspection' build configuration were updated

--- .teamcity/patches/buildTypes/ResharperInspection.kts
+++ .teamcity/patches/buildTypes/ResharperInspection.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("ResharperInspection")
     name = "resharper inspection"
+
+    vcs {
+        root(RelativeId("HttpsGithubComSkrysmanskiResharperInspectcodeToolsetbug"))
+    }
 }))
 
