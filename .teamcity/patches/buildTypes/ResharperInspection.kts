05ab1ca73d748c58ffb19ce5be90f34fd9763ad5
admin <admin@TeamCity>
2021-07-01 10:09:27 +0200

    TeamCity change in 'resharper duplicates' project: runners of 'resharper inspection' build configuration were updated

--- .teamcity/patches/buildTypes/ResharperInspection.kts
+++ .teamcity/patches/buildTypes/ResharperInspection.kts
@@ -20,7 +20,11 @@ create(DslContext.projectId, BuildType({
     steps {
         step {
             type = "dotnet-tools-inspectcode"
-            param("dotnet-tools-inspectcode.customCmdArgs", "-o=reports/cs-report.xml --no-swea --exclude=**/*.ts")
+            param("dotnet-tools-inspectcode.customCmdArgs", """
+                -o=reports/cs-report.xml 
+                --no-swea 
+                --exclude=**/*.ts
+            """.trimIndent())
             param("dotnet-tools-inspectcode.solution", "VisualStudioToolSetBug.sln")
             param("jetbrains.resharper-clt.platform", "x64")
             param("jetbrains.resharper-clt.clt-path", "%teamcity.tool.jetbrains.resharper-clt.DEFAULT%")
